bfb3c2c2dabe058f38ebd9a4810097207d28d0eb
crollinson <crolli@comm675-0401-dhcp161.bu.edu>
2015-01-30 15:10:36 -0500

    Original format_output
    Original format_output.sh from Jackie

--- format_output.sh
+++ format_output.sh
@@ -10,7 +10,7 @@ vars=(lwdown precipf qair swdown tair)
 
 #First merge the qair bias correction into 1 file
 pushd ${out_dir}
-#cdo mergetime qair_*_corr.nc qair_bias_corr.nc
+cdo mergetime qair_*.nc qair_bias_corr.nc
 
 #Make directories for organization
 if [ ! -d ${yr_dir} ]
@@ -30,23 +30,14 @@ done
 #Then split the files into years & then months
 for var in ${vars[@]}
 do
-    echo $var
-    pushd ${yr_dir}${var}/
-    cp ${out_dir}
-    cdo splityear ${var}_bias_corr.nc ${yr_dir}${var}/${var}_
+    cdo splityear ${var}_bias_corr.nc ${yr_dir}/${var}/${var}_
+    for file in ${yr_dir}/${var}/*.nc
+    do
+	year=`echo $file | tail -c 8 | cut -c1-4`
+	echo ${year}
+
+    cdo splitmon ${yr_dir}/${var}/${var}* 
 done
 
-for var in ${vars[@]}
-do
-    echo $var
-    for file in ${yr_dir}${var}/*.nc
-    do
-        echo $file
-        year=`echo $file | tail -c 8 | cut -c1-4`
-        echo ${year}
-        cdo splitmon $file ${yr_dir}${var}/${var}_${year}_*
-    done
-    rm -f ${yr_dir}${var}/${var}_[0-9][0-9][0-9][0-9].nc
-done
 
 popd
