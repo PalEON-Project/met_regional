30aa6c8c8bbb858b70712764a42386adb823dd16
crollinson <crolli@comm675-0401-dhcp161.bu.edu>
2015-01-30 14:49:02 -0500

    Update Parse_Sites
    Update Parse sites to include all variables and all sites

--- parse_sites.sh
+++ parse_sites.sh
@@ -1,21 +1,30 @@
 #!bin/bash
 #This code loops through files and separates out met data for the 
-#sites Harvard Forest (Ha1), Howland Forest (Ho1), and UNDERC (Und)
+#sites: Harvard Forest (PHA), Howland Forest (PHO), UNDERC (PUN)
+#		Billy's Lake (PBL), Demming Lake (PDL), and Minden Bog (PMB)
 #Jaclyn Hatala Matthes, 1/14/14
 #jaclyn.hatala.matthes@gmail.com
 
-in_dir=/projectnb/dietzelab/paleon/met_regional/bias_corr/
-out_dir=/projectnb/dietzelab/paleon/met_regional/bias_corr/sites/
+in_dir=/projectnb/dietzelab/paleon/met_regional/bias_corr/regional_monthly/
+out_dir=/projectnb/dietzelab/paleon/met_regional/bias_corr/regional_monthly/sites/
 
-vars=(psurf)
-
-pushd ${in_dir}
+vars=(lwdown precipf psurf qair swdown tair wind)                                                      
 
 for var in ${vars[@]}
 do
-    ncea -O -d lat,42.5,43.0 -d lon,-72.5,-72.0 ${var}_gcm_cat.nc ${out_dir}Ha1_${var}_cat.nc
-    ncea -O -d lat,45.0,45.5 -d lon,-69.0,-68.5 ${var}_gcm_cat.nc ${out_dir}Ho1_${var}_cat.nc
-    ncea -O -d lat,46.0,46.5 -d lon,-89.5,-89.0 ${var}_gcm_cat.nc ${out_dir}Und_${var}_cat.nc
+    pushd ${in_dir}${var}/
+    for file in *.nc
+    do
+	ncea -O -d lat,42.5,43.0 -d lon,-72.5,-72.0 $file ${out_dir}PHA/${var}/PHA_$file
+	ncea -O -d lat,45.0,45.5 -d lon,-69.0,-68.5 $file ${out_dir}PHO/${var}/PHO_$file
+	ncea -O -d lat,46.0,46.5 -d lon,-89.5,-89.0 $file ${out_dir}PUN/${var}/PUN_$file
+	ncea -O -d lat,46.0,46.5 -d lon,-95.0,-94.5 $file ${out_dir}PBL/${var}/PBL_$file
+	ncea -O -d lat,47.0,47.5 -d lon,-95.5,-95.0 $file ${out_dir}PDL/${var}/PDL_$file
+	ncea -O -d lat,43.5,44.0 -d lon,-83.0,-82.5 $file ${out_dir}PMB/${var}/PMB_$file
+
+    done
+    popd
+
 done
 
 
