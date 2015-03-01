e55c446fa7d021a025616541e6cc89eaf19a21b4
crollinson <crollinson@gmail.com>
2015-03-01 00:07:53 -0700

    Script to compress everything into a single TAR

---
+++ compress_files_singletar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#$ -wd /projectnb/dietzelab/paleon/met_regional/
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+#$ -m e
+#$ -M crollinson@gmail.com
+#$ -l h_rt=24:00:00
+#$ -N CompressMet
+#cd /projectnb/dietzelab/paleon/met_regional/
+tar -jcvf phase1a_met_drivers_v4.1.tar.bz2 phase1a_met_drivers_v4.1
