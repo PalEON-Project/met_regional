98c61c173d541f10964d1d61f2020c500b1fbb00
crollinson <crollinson@gmail.com>
2015-09-22 12:42:41 -0400

    tweak again to get things to run

--- 8as_format_output.sh
+++ 8as_format_output.sh
@@ -1,4 +1,3 @@
-# Script transfers & packages final bias-corrected output to folder for distribution
 #!/bin/sh
 #$ -wd /projectnb/dietzelab/paleon/met_regional/
 #$ -j y
@@ -9,5 +8,6 @@
 #$ -l h_rt=60:00:00
 #$ -N Pack_Subday
 #cd /projectnb/dietzelab/paleon/met_regional/
+# Script transfers & packages final bias-corrected output to folder for distribution
 
 sh 8a_format_output.sh
