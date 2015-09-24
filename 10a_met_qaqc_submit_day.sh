296e1f3ea35d9d3083869758d927d9a4a3585884
crollinson <crollinson@gmail.com>
2015-09-24 12:50:33 -0400

    name changes (day to subday), add actual day QA/QC

---
+++ 10a_met_qaqc_submit_day.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#$ -wd /projectnb/dietzelab/paleon/met_regional/
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+#$ -m e
+#$ -M crollinson@gmail.com
+#$ -l h_rt=48:00:00
+#$ -N DayQAQC
+#cd /projectnb/dietzelab/paleon/met_regional/
+R CMD BATCH 10_qaqc_check_regional_day.R
--- 7cs_met_qaqc_submit_subday.sh
+++ 7cs_met_qaqc_submit_subday.sh
@@ -6,6 +6,6 @@
 #$ -m e
 #$ -M crollinson@gmail.com
 #$ -l h_rt=48:00:00
-#$ -N DayQAQC
+#$ -N SubDayQAQC
 #cd /projectnb/dietzelab/paleon/met_regional/
-R CMD BATCH 7c_qaqc_check_regional_day.R
+R CMD BATCH 7c_qaqc_check_regional_subday.R
